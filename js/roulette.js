c759b4c3b6bdb2aa01a9b85edd2d4c1f065e3fd9
JoonHo Park <pjhjohn@gmail.com>
2017-03-23 15:27:09 +0900

    Follow JavaScript CodeStyle
    Rename Parameter Name
    - renderCallback -> onRenderRoulette
    - stopCallback -> onStopRoulette
    - option <-> config
    - roulette holds onRenderCallback and onStopCallback
    Style
    - Add Semicolons
    - Remove Comma on Last Item

--- js/roulette.js
+++ js/roulette.js
@@ -1,42 +1,42 @@
-var Roulette = function(imageElement, canvasElement, renderCallback, stopCallback, config) {
+var Roulette = function(imageElement, canvasElement, onRenderRoulette, onStopRoulette, option) {
   var ctx;
   var image = imageElement;
   var canvas = canvasElement;
-  var renderCallback = renderCallback;
-  var stopCallback = stopCallback;
-  var option = {
+  var config = {
     stepSize: 30, // ms
     maxVelocity: 30.0, // degrees/step
     spinAcceleration: 1.5, // degrees/step/step
     easeOutAcceleration: -1.0, // degrees/step/step
-    requestTimeOut: 3000, // ms
-  }
-  if (config) {
-    option.stepSize = config.stepSize||option.stepSize;
-    option.maxVelocity = config.maxVelocity||option.maxVelocity;
-    option.spinAcceleration = config.spinAcceleration||option.spinAcceleration;
-    option.easeOutAcceleration = config.easeOutAcceleration||option.easeOutAcceleration;
-    option.requestTimeOut = config.requestTimeOut||option.requestTimeOut;
+    requestTimeOut: 3000 // ms
+  };
+  if (option) {
+    config.stepSize = option.stepSize||config.stepSize;
+    config.maxVelocity = option.maxVelocity||config.maxVelocity;
+    config.spinAcceleration = option.spinAcceleration||config.spinAcceleration;
+    config.easeOutAcceleration = option.easeOutAcceleration||config.easeOutAcceleration;
+    config.requestTimeOut = option.requestTimeOut||config.requestTimeOut;
   }
   var roulette = {
+    onRenderCallback: onRenderRoulette,
+    onStopCallback: onStopRoulette,
     angle: 0, // degrees
     velocity: 0, // degrees/step
     elapsedTime: 0, // ms
     spinning: false
-  }
+  };
 
   var angle = function() {
     return roulette.angle;
-  }
+  };
   var velocity = function() {
     return roulette.velocity;
-  }
+  };
   var elapsedTime = function() {
     return roulette.elapsedTime;
-  }
+  };
   var spinning = function() {
     return roulette.spinning;
-  }
+  };
 
   var start = function() {
     if (roulette.spinning) {
@@ -47,7 +47,7 @@ var Roulette = function(imageElement, canvasElement, renderCallback, stopCallbac
       roulette.spinning = true;
       startRoulette();
     }
-  }
+  };
 
   var stop = function() {
     if (!roulette.spinning) {
@@ -56,54 +56,54 @@ var Roulette = function(imageElement, canvasElement, renderCallback, stopCallbac
       console.log("Stop Roulette");
       roulette.spinning = false;
     }
-  }
+  };
 
   var startRoulette = function () {
     if (!roulette.spinning) {
       stopRoulette();
     } else {
       roulette.angle += roulette.velocity;
-      roulette.velocity += option.spinAcceleration;
-      if (roulette.velocity > option.maxVelocity) roulette.velocity = option.maxVelocity;
+      roulette.velocity += config.spinAcceleration;
+      if (roulette.velocity > config.maxVelocity) roulette.velocity = config.maxVelocity;
       render(roulette.angle);
       setTimeout(function() {
-        roulette.elapsedTime += option.stepSize;
+        roulette.elapsedTime += config.stepSize;
         startRoulette();
-      }, option.stepSize);
+      }, config.stepSize);
     }
-  }
+  };
 
   var stopRoulette = function() {
     if (roulette.velocity < 0) {
       roulette.velocity = 0;
-      if (stopCallback) stopCallback({
+      if (roulette.onStopCallback) roulette.onStopCallback({
         angle: roulette.angle,
         velocity: roulette.velocity,
         elapsedTime: roulette.elapsedTime,
         spinning: roulette.spinning,
-        stepSize: option.stepSize,
+        stepSize: config.stepSize
       });
     } else {
       roulette.angle += roulette.velocity;
-      roulette.velocity += option.easeOutAcceleration;
+      roulette.velocity += config.easeOutAcceleration;
       render(roulette.angle);
       setTimeout(function() {
-        roulette.elapsedTime += option.stepSize;
+        roulette.elapsedTime += config.stepSize;
         stopRoulette();
-      }, option.stepSize);
+      }, config.stepSize);
     }
 
     /* Initial Render */
     render(roulette.angle);
-  }
+  };
 
   var render = function(degrees) {
-    if (renderCallback) renderCallback({
+    if (roulette.onRenderCallback) roulette.onRenderCallback({
       angle: roulette.angle,
       velocity: roulette.velocity,
       elapsedTime: roulette.elapsedTime,
       spinning: roulette.spinning,
-      stepSize: option.stepSize,
+      stepSize: config.stepSize
     });
     if (canvas.getContext) {
       ctx = canvas.getContext("2d");
@@ -114,7 +114,7 @@ var Roulette = function(imageElement, canvasElement, renderCallback, stopCallbac
       ctx.drawImage(image, 0, 0, image.width, image.height, -canvas.width/2, -canvas.height/2, canvas.width, canvas.height);
       ctx.restore();
     }
-  }
+  };
 
   render(roulette.angle);
 
@@ -124,6 +124,6 @@ var Roulette = function(imageElement, canvasElement, renderCallback, stopCallbac
     elapsedTime: elapsedTime,
     spinning: spinning,
     start: start,
-    stop: stop,
+    stop: stop
   }
 };
